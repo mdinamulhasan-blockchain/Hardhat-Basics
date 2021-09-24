ab781388dfd9fc8f9635e91fa30c9cd334a5c2fb
Md Inamul Hasan <85017600+mdinamulhasan-blockchain@users.noreply.github.com>
2021-09-24 17:38:33 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@
 
  const INFURA_URL = "https://ropsten.infura.io/v3/5a80acc9d0814d4c9334e75b33606801";
  
- const ROPSTEN_PRIVATE_KEY = "b634a45c2ad1b9113d37230d390a58637d548809b626f2d1dfdc0da3d720d138";
+ const ROPSTEN_PRIVATE_KEY = "****************************************************************";
 
 module.exports = {
   solidity: "0.7.3",
